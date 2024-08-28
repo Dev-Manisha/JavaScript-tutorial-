0a5b6d9c26cda232769264d4b2443b207326cc48
Manisha Sharma <88490970+Dev-Manisha@users.noreply.github.com>
2024-08-28 11:31:27 +0000

    datatypes done

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,20 @@
+" use strict"; // trat all JS code as new version
+
+//alert( 3 + 3 ) // we are using nodejs, not browser
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//  null => standalone value
+// undefind
+// symbol => unique
+
+
+// object
+
+console.log(typeof "manisha"); //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
